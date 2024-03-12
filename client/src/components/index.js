e8fb8e847293848ec169bbd267674aae093519fc
=Meltus645 <=meltusjuma5@gmail.com>
2024-03-13 01:19:55 +0300

    An entry point for components

---
+++ client/src/components/index.js
@@ -0,0 +1,4 @@
+export {default as Form} from './Form';
+export {default as Table} from './Table';
+export {default as Loader} from './SpinLoader';
+export {default as Toast} from './ToastNotification';
